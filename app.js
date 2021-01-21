8eeaf653e442f6313709d2b5969f75ba60e163e0
Aarika Turner <turneraarika@gmail.com>
2021-01-21 09:08:22 -0500

    push

---
+++ app.js
@@ -0,0 +1,22 @@
+var newVar1 = 1
+newVar1
+var newVar2 = 2
+var newVar3 =3
+var newVar4 =4
+var newVar5 =5
+var newVar6 = 6
+var newVar7 = 7
+var newVar8 = 8
+var newVar9 = 9
+var newVar10 = 10
+
+console.log(newVar1 + newVar2);
+console.log(newVar3 + newVar4);
+console.log(newVar10 - newVar9);
+console.log(newVar8 - newVar7);
+console.log(newVar5*newVar6);
+console.log(newVar5*newVar4);
+++newVar5
+++newVar10
+--newVar9
+--newVar7
